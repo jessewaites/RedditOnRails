bcf60492ea1ebc4db8588916d0478908b1c0aa57
Jesse Waites <jesse.waites@gmail.com>
2013-09-15 16:25:15 -0500

    heroku fix

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -7,6 +7,8 @@ ActiveAdmin.setup do |config|
   #
   config.site_title = "Reddit Rails"
 
+  config.default_namespace = :activeadmin # Default :admin
+
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
