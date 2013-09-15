25bf9cf5ad71fced245c7f59252b49b8ca5acf3e
Jesse Waites <jesse.waites@gmail.com>
2013-09-15 15:55:29 -0500

    attempting to deploy activeadmin to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ RedditRails::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.precompile += ['active_admin.js', 'active_admin.css']
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
